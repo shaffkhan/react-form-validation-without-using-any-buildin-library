6a4b3a7bf74b59d4773738a8a6be58062d3e633d
Shaff Khan <shaffkhany@gmail.com>
2022-12-22 10:36:15 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [input, setInput] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  const inputHandler = e => {
+      setInput(e.target.value);
+  };
+
+  useEffect(() => {
+      if (input.length < 5 || /\d/.test(input)) {
+          setIsValid(false);
+      } else {
+          setIsValid(true);
+      }
+  }, [input]);
+  
+   
+
+    return (
+        <>
+            <hr />
+            <h2>useEffect use case</h2>
+            <h3>Running on state change: validating input field</h3>
+            <form>
+                <label htmlFor="input">Write something (more than 5 non numerical characters is a valid input)</label><br />
+                <input type="text" id="input" autoComplete="off" onChange={inputHandler} style={{ height: '1.5rem', width: '20rem', marginTop: '1rem' }} />
+            </form>
+            <p><span style={isValid ? { backgroundColor: 'lightgreen', padding: '.5rem' } : { backgroundColor: 'lightpink', padding: '.5rem' }}>{isValid ? 'Valid input' : 'Input not valid'}</span></p>
+        </>
+    );
+
 }
 
 export default App;
